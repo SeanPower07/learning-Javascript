4ff9aed022e099aebc3781e3200e532a9ee50203
SeanPower07 <41095097+SeanPower07@users.noreply.github.com>
2018-07-18 03:38:51 -0400

    Revert "addition.js"
    This reverts commit bb3c2cde58b6106b179654b8624a081ccf4e5f08.

--- addition.js
+++
@@ -1,4 +0,0 @@
-var firstNumber = prompt('give me your first number please');
-var secondNumber = prompt('and your second number please');
-alert(+firstNumber + +secondNumber);
-// Why does this work in Browser console but not in GitBash?
