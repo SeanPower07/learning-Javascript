f0611724370ca7aadd731f075e446b309cde59e6
SeanPower07 <41095097+SeanPower07@users.noreply.github.com>
2018-07-18 04:55:51 -0400

    Friends prompt example

---
+++ Prompt Friends.js
@@ -0,0 +1,8 @@
+var friends = prompt("How many friends do you REALLY have?");
+if (friends == 1) {
+ alert("You seem to be honest with yourself.");  
+} else if (friends <= 3) {
+alert("You have a better grasp on reality than most, congratulations!");
+} else {
+alert("Why lie to a machine?");
+}
