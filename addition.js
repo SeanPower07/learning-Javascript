b3bb69265e2ca6545c5a1441771e2ad314844c45
SeanPower07 <41095097+SeanPower07@users.noreply.github.com>
2018-07-18 04:29:03 -0400

    Addition ex

---
+++ addition.js
@@ -0,0 +1,4 @@
+var firstNumber = prompt('give me your first number please');
+var secondNumber = prompt('and your second number please');
+alert(+firstNumber + +secondNumber);
+// why doesn't this work in GitBash but works in console
