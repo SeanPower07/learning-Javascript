35eaf012ee2f3f9c0b69adabd6eb1bdc3f460ac3
SeanPower07 <41095097+SeanPower07@users.noreply.github.com>
2018-07-18 04:25:23 -0400

    Prompt Intro Gaming example

---
+++ Prompt Games.js
@@ -0,0 +1,6 @@
+var years = prompt('How many years have you played competitive games?');
+if (years < 5) {
+alert("You're probably a scrub.")
+} else {
+alert("Submit an application to join our competitive gaming community!")
+}
