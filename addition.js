b4ebd038ca170767a0a2cfc30b493bd0017d5818
SeanPower07 <41095097+SeanPower07@users.noreply.github.com>
2018-07-18 07:21:04 -0400

    Changed multiple things around added comments

--- addition.js
+++ addition.js
@@ -1,4 +1,8 @@
+//var firstNumber = prompt('give me your first number please');
+//var secondNumber = prompt('and your second number please');
+//alert(+firstNumber + +secondNumber);
+// why doesn't this work in GitBash but works in console
+// did not work because alert is a console command, you have to use console.log otherwise
 var firstNumber = prompt('give me your first number please');
 var secondNumber = prompt('and your second number please');
-alert(+firstNumber + +secondNumber);
-// why doesn't this work in GitBash but works in console
+console.log(+firstNumber + +secondNumber);
--- function-addition.js
+++ function-addition.js
@@ -3,3 +3,4 @@ function add(first, second) {
 }
 
 console.log( add(1, 2) );
+// did not work with alert command, have to use console.log for git-bash
