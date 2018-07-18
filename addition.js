bb3c2cde58b6106b179654b8624a081ccf4e5f08
SeanPower07 <41095097+SeanPower07@users.noreply.github.com>
2018-07-18 03:37:16 -0400

    addition.js
    worked in chrome, didn't work in gitbash

---
+++ addition.js
@@ -0,0 +1,4 @@
+var firstNumber = prompt('give me your first number please');
+var secondNumber = prompt('and your second number please');
+alert(+firstNumber + +secondNumber);
+// Why does this work in Browser console but not in GitBash?
